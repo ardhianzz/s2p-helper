32ca2f8832f94926a6f391267e93eb30bbe8cddb
Nur Ardhiansyah <ardhianzz@gmail.com>
2022-09-29 10:20:17 +0700

    test pull

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,15 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use App\Models\HakAkses;
-use App\Models\Modul;
-use App\Models\PegawaiLevelUser;
-use App\Models\Pegawai;
-use App\Models\Jabatan;
-use App\Models\Divisi;
-use App\Models\Lembur;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
