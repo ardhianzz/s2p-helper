ebd1782fce7aa77aad9abd7aef74b8ba5134e5e7
lattif-mac <lattif-mac>
2022-11-08 13:32:28 +0700

    Mencoba ip public

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,7 +31,7 @@ class LoginController extends Controller
             $data_ip = DB::table("users")->where("id", auth()->user()->id)->get()[0]->last_login_ip;
             $data_waktu = DB::table("users")->where("id", auth()->user()->id)->get()[0]->last_login_at;
             $check = geoip()->getLocation($_SERVER['REMOTE_ADDR']);
-            $ip = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
+            // $ip = trim(shell_exec("dig +short myip.opendns.com @resolver1.opendns.com"));
             $request->session()->regenerate();
             $details = [
                 'title' => 'Notifikasi Keamanan',
@@ -41,7 +41,7 @@ class LoginController extends Controller
                 ];
                
                 // Mail::to($request->email)->send(new notif_login($details));
-                dd("Public IP: ".$ip);
+                dd($check);
 
                 
             return redirect()->intended("/main");
